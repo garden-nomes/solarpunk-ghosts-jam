5b5b8f14cc845bdcaffc0467c13865c9f7404da9
MineralPass <bethwing89@gmail.com>
2021-07-22 20:01:55 -0700

    Camera Follow Script with Transform Reference The camera LateUpdate function was modified to use the new player_tf object which references the player tf.
    The camera mode variable was made public, and the global transform
    reference, 'player_tf,' was created.

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -12,7 +12,11 @@ enum CameraMode
 
 public class Camera : MonoBehaviour
 {
-    CameraMode mode = CameraMode.Follow;
+
+    public Transform player_tf;
+
+
+    public CameraMode mode = CameraMode.Follow;
 
     // Update is called once per frame
     void LateUpdate()
@@ -22,8 +26,7 @@ public class Camera : MonoBehaviour
             case CameraMode.Static:
                 return;
             case CameraMode.Follow:
-                GameObject player = GameObject.Find("Player");
-                transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+                transform.position = new Vector3(player_tf.position.x, player_tf.transform.position.y, -10);
                 break;
         }
     }
