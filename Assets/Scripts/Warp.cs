28543cfa014a52b8248c1e2da9da28147f1ff581
MineralPass <bethwing89@gmail.com>
2021-07-28 22:34:33 -0700

    First Draft of Warp 'warp.cs' created, which is a monobehavior which calls 'OnTriggerStay2D.' class warp has a variable, 'openness,' which indicates if the warp is 'open' (true) or 'closed.' (false)
    warp.cs needs debugging. The intent is for a message to be posted to the
    console when 'openness' is marked as true, the player is colliding with
    'entrance,' and the key 'e' is pressed.
    
    A new object, 'entrance,' was added to the project for debugging
    'warp.cs.'
    
    The player layer level was changed to '1' so it is always drawn over the
    warp and other objects.
    
    A 2D box collider was added to 'entrance' and 'player' to realize the
    warp design.

---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+    // describes whether the warp is open or closed
+    public bool openness = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        // check for user input
+        if (other.CompareTag("Player") && openness && Input.GetKeyDown("e"))
+            // open new scene
+            System.Console.WriteLine("The user requested to open up the scene.");
+            
+
+    }
+}
