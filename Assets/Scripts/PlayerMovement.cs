610aa2d493fd38029224d163a15a2407837f04f2
MineralPass <bethwing89@gmail.com>
2021-07-29 16:01:14 -0700

    'PlayerMovement.cs' Anim Routine Draft Some logic, an enum, and some variables have been added to the 'PlayerMovement.cs' script as part of an animation routine.
    I removed some sprites and some other animation structures which I was
    not immediately using.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
+
+// PlayerStatus indicates whether the player is moving or standing  
+// The order and value of the status declarations are used to compute the animation frame
+public enum PlayerStatus : int
+{
+    Walking_Towards, Walking_Right, Walking_Away, Walking_Left, Facing_Towards, Facing_Right, Facing_Away, Facing_Left
+}
+
 public class PlayerMovement : MonoBehaviour
 {
     // TODO: Collisions, or any kind of physics really. I'm not sure yet whether we're using the 2D
@@ -7,6 +15,19 @@ public class PlayerMovement : MonoBehaviour
     // coordinate system.
 
     public float moveSpeed = 3f;
+
+    public PlayerStatus prevStatus = PlayerStatus.Facing_Towards;
+    public PlayerStatus currStatus = PlayerStatus.Facing_Towards;
+
+    public float animationSpeed = 1.0f;
+
+    public Texture2D[] frames;
+
+
+    private void Start()
+    {
+        
+    }
 
     void Update()
     {
@@ -16,10 +37,36 @@ public class PlayerMovement : MonoBehaviour
             return;
         }
 
-        // simple 2D movement
+
+        // get player velocity from input
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        // check if the player is standing
+        if (horizontal == 0.0f && horizontal == vertical)
+        {
+            if ((int) currStatus >= (int) PlayerStatus.Facing_Towards)
+                return;
+            
+            // modify the active texture  and player status if the player has changed from moving to standing
+        }
+
+        if (Mathf.Abs(vertical) > Mathf.Abs(horizontal))
+        {
+            // vert is greater than horiz; the player is moving either towards or away from the camera
+           
+            if (vertical < 0f)
+                // the player is moving towards the camera
+                ;
+        }
+        else
+        {
+            // horiz is greater than or equal to vert
+        }
+
         var velocity = Vector2.ClampMagnitude(new Vector2(horizontal, vertical), 1f) * moveSpeed;
+
+
         transform.position += (Vector3) velocity * Time.deltaTime;
     }
 }
